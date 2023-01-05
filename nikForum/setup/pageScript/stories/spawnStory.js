15dcc0b6441079088cee85a2f6ef006dfda92972
perinaSoba <perinasoba@gmail.com>
2023-01-05 20:37:00 +0100

    VS Code Edit

--- nikForum/setup/pageScript/stories/spawnStory.js
+++ nikForum/setup/pageScript/stories/spawnStory.js
@@ -1,7 +1,29 @@
 fetch('https://perinasoba.github.io/nikForum/setup/stories.json')
 .then(response=> response.json())
 .then((storiesJson) => {
-    var storyTitle = document.getElementsByTagName("META")[0].content;
+    function parseURLParams(url) {
+        var queryStart = url.indexOf("?") + 1,
+            queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+            query = url.slice(queryStart, queryEnd - 1),
+            pairs = query.replace(/\+/g, " ").split("&"),
+            parms = {}, i, n, v, nv;
+
+        if (query === url || query === "") return;
+
+        for (i = 0; i < pairs.length; i++) {
+            nv = pairs[i].split("=", 2);
+            n = decodeURIComponent(nv[0]);
+            v = decodeURIComponent(nv[1]);
+
+            if (!parms.hasOwnProperty(n)) parms[n] = [];
+            parms[n].push(nv.length === 2 ? v : null);
+        } 
+        return parms;
+    };
+
+    var urlData = parseURLParams(`${document.location.href}`);
+
+    var storyTitle = urlData.id[0];
 
     var storyArray = storiesJson.filter(function (el) {
         return el.title ==  storyTitle;
@@ -10,7 +32,7 @@ fetch('https://perinasoba.github.io/nikForum/setup/stories.json')
     var storyObject = storyArray[0];
 
     document.title = "Nik Forum - " + storyObject.title;
-
+    
     document.getElementById("title").innerHTML = storyObject.title;
     document.getElementById("authImg").src = storyObject.author.profile_pic;
     document.getElementById("authName").innerHTML = storyObject.author.name;
