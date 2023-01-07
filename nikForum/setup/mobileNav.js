2b99f364de1a26ef636dda900466224012b6e893
perinaSoba <perinasoba@gmail.com>
2023-01-07 20:31:08 +0100

    VS Code Edit

--- nikForum/setup/mobileNav.js
+++
@@ -1,9 +0,0 @@
-function buttPressed() {
-    if (window.getComputedStyle(document.getElementById("mobileNav"), null).display == "none") {
-        document.getElementById("mobileNav").style.display = "block";
-        document.getElementById("mobNavButt").style.background = "transparent";
-    } else if (window.getComputedStyle(document.getElementById("mobileNav"), null).display == "block") {
-        document.getElementById("mobileNav").style.display = "none";
-        document.getElementById("mobNavButt").style.background = "#fff";
-    }
-}
