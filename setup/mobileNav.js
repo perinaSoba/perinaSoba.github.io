259fbffe453970c37c16a7f3a96a46be692dff7d
perinaSoba <perinasoba@gmail.com>
2023-01-07 18:23:38 +0100

    VS Code Edit

--- setup/mobileNav.js
+++ setup/mobileNav.js
@@ -2,8 +2,10 @@ function buttPressed() {
     if (window.getComputedStyle(document.getElementById("mobileNav"), null).display == "none") {
         document.getElementById("mobileNav").style.display = "block";
         document.getElementById("mobNavButt").style.background = "transparent";
+        document.getElementById("movLogInDiv").style.display = "none";
     } else if (window.getComputedStyle(document.getElementById("mobileNav"), null).display == "block") {
         document.getElementById("mobileNav").style.display = "none";
         document.getElementById("mobNavButt").style.background = "#fff";
+        document.getElementById("movLogInDiv").style.display = "block";
     }
 }
