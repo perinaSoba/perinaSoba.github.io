5a746a33b9e939afa0a78a93438fb1faeee958c2
perinaSoba <perinasoba@gmail.com>
2023-01-14 23:00:51 +0100

    VS Code Edit

--- setup/settings.js
+++ setup/settings.js
@@ -27,12 +27,40 @@ if (getCookie(`tema`) == null) {
         star.innerHTML = '*{color: #e3e3e3!important;}';
         document.body.append(star);
 
+        const pageContent = document.createElement('style'); 
+        pageContent.innerHTML = '#pageContent{background-color: #201c1c;}';
+        document.body.append(pageContent);
+
         const sideBar = document.createElement('style'); 
         sideBar.innerHTML = '#sideBar{background-color: #2d2f31!important;}';
         document.body.append(sideBar);
 
-        const activeCard_img = document.createElement('style'); 
-        activeCard_img.innerHTML = '#activeCard img{background-color: #10547c!important; fill: currentColor;}';
-        document.body.append(activeCard_img);
+        const material_symbols_rounded = document.createElement('style'); 
+        material_symbols_rounded.innerHTML = '.material-symbols-rounded{color: #c4c7c5!important;}';
+        document.body.append(material_symbols_rounded);
+
+        const sideButtons_button_HOVER_span = document.createElement('style'); 
+        sideButtons_button_HOVER_span.innerHTML = '#sideButtons button:hover span {background-color: #383a3c!important;}';
+        document.body.append(sideButtons_button_HOVER_span);
+
+        const activeCard_span = document.createElement('style'); 
+        activeCard_span.innerHTML = '#activeCard span{background-color: #0f537d!important; color: #c2e7ff!important;}';
+        document.body.append(activeCard_span);
+
+        const pgBanner_h1 = document.createElement('style'); 
+        pgBanner_h1.innerHTML = '#pgBanner h1{color: #fff!important;}';
+        document.body.append(pgBanner_h1);
+
+        const logInDiv = document.createElement('style'); 
+        logInDiv.innerHTML = '#logInDiv{border: 0.1rem #8b8e8c solid!important; background-color: transparent;}';
+        document.body.append(logInDiv);
+        
+        const textBox_a = document.createElement('style'); 
+        textBox_a.innerHTML = '.textBox a{color: #a8c7fa!important;}';
+        document.body.append(textBox_a);
+        
+        const userInfoCard_userImage = document.createElement('style'); 
+        userInfoCard_userImage.innerHTML = '#userInfoCard #userImage{border: 0.2rem solid #0f537d;}';
+        document.body.append(userInfoCard_userImage);
     }
 }
