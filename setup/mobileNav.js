9af75bd14e5a9ca6aedc584253687a6b97a14f15
perinaSoba <perinasoba@gmail.com>
2023-01-15 19:42:53 +0100

    VS Code Edit

--- setup/mobileNav.js
+++ setup/mobileNav.js
@@ -34,10 +34,8 @@ function buttPressed() {
     if (window.getComputedStyle(document.getElementById("mobileNav"), null).display == "none") {
         document.getElementById("mobileNav").style.display = "block";
         document.getElementById("mobNavButt").style.background = "transparent";
-        document.getElementById("mobLogInDiv").style.display = "none";
     } else if (window.getComputedStyle(document.getElementById("mobileNav"), null).display == "block") {
         document.getElementById("mobileNav").style.display = "none";
         document.getElementById("mobNavButt").style.background = "#fff";
-        document.getElementById("mobLogInDiv").style.display = "block";
     }
 }
--- setup/settings.js
+++ setup/settings.js
@@ -21,44 +21,104 @@ function getCookie(name) {
 
 function blackThemeSetup() {
     const star = document.createElement('style'); 
-        star.innerHTML = '*{color: #e3e3e3!important;}';
-        document.body.append(star);
+    star.innerHTML = '*{color: #e3e3e3!important;}';
+    document.body.append(star);
 
-        const pageContent = document.createElement('style'); 
-        pageContent.innerHTML = '#pageContent{background-color: #201c1c;}';
-        document.body.append(pageContent);
+    const pageContent = document.createElement('style'); 
+    pageContent.innerHTML = '#pageContent{background-color: #201c1c;}';
+    document.body.append(pageContent);
 
-        const sideBar = document.createElement('style'); 
-        sideBar.innerHTML = '#sideBar{background-color: #2d2f31!important;}';
-        document.body.append(sideBar);
+    const sideBar = document.createElement('style'); 
+    sideBar.innerHTML = '#sideBar{background-color: #2d2f31!important;}';
+    document.body.append(sideBar);
 
-        const material_symbols_rounded = document.createElement('style'); 
-        material_symbols_rounded.innerHTML = '.material-symbols-rounded{color: #c4c7c5!important;}';
-        document.body.append(material_symbols_rounded);
+    const mobNavButt = document.createElement('style'); 
+    mobNavButt.innerHTML = '#mobNavButt{background-color: #2d2f31!important;}';
+    document.body.append(mobNavButt);
 
-        const sideButtons_button_HOVER_span = document.createElement('style'); 
-        sideButtons_button_HOVER_span.innerHTML = '#sideButtons button:hover span {background-color: #383a3c!important;}';
-        document.body.append(sideButtons_button_HOVER_span);
+    const mobileNav = document.createElement('style'); 
+    mobileNav.innerHTML = '#mobileNav{background-color: #2d2f31!important;}';
+    document.body.append(mobileNav);
 
-        const activeCard_span = document.createElement('style'); 
-        activeCard_span.innerHTML = '#activeCard span{background-color: #0f537d!important; color: #c2e7ff!important;}';
-        document.body.append(activeCard_span);
+    const mobileNav_currPageMob = document.createElement('style'); 
+    mobileNav_currPageMob.innerHTML = '#mobileNav #currPageMob{background-color: #0f537d!important; color: #bce2fa;}';
+    document.body.append(mobileNav_currPageMob);
 
-        const pgBanner_h1 = document.createElement('style'); 
-        pgBanner_h1.innerHTML = '#pgBanner h1{color: #fff!important;}';
-        document.body.append(pgBanner_h1);
+    const mobileNav_a_HOVER = document.createElement('style'); 
+    mobileNav_a_HOVER.innerHTML = '#mobileNav a:hover{background-color: #383a3c!important; color: #bce2fa;}';
+    document.body.append(mobileNav_a_HOVER);
 
-        const logInDiv = document.createElement('style'); 
-        logInDiv.innerHTML = '#logInDiv{border: 0.1rem #8b8e8c solid!important; background-color: transparent;}';
-        document.body.append(logInDiv);
-        
-        const textBox_a = document.createElement('style'); 
-        textBox_a.innerHTML = '.textBox a{color: #a8c7fa!important;}';
-        document.body.append(textBox_a);
-        
-        const userInfoCard_userImage = document.createElement('style'); 
-        userInfoCard_userImage.innerHTML = '#userInfoCard #userImage{border: 0.2rem solid #0f537d;}';
-        document.body.append(userInfoCard_userImage);
+    const material_symbols_rounded = document.createElement('style'); 
+    material_symbols_rounded.innerHTML = '.material-symbols-rounded{color: #c4c7c5!important;}';
+    document.body.append(material_symbols_rounded);
+
+    const sideButtons_button_HOVER_span = document.createElement('style'); 
+    sideButtons_button_HOVER_span.innerHTML = '#sideButtons button:hover span {background-color: #383a3c!important;}';
+    document.body.append(sideButtons_button_HOVER_span);
+
+    const activeCard_span = document.createElement('style'); 
+    activeCard_span.innerHTML = '#activeCard span{background-color: #0f537d!important; color: #c2e7ff!important;}';
+    document.body.append(activeCard_span);
+
+    const pgBanner_h1 = document.createElement('style'); 
+    pgBanner_h1.innerHTML = '#pgBanner h1{color: #fff!important;}';
+    document.body.append(pgBanner_h1);
+
+    const logInDiv = document.createElement('style'); 
+    logInDiv.innerHTML = '#logInDiv{border: 0.1rem #8b8e8c solid!important; background-color: transparent;}';
+    document.body.append(logInDiv);
+    
+    const textBox_a = document.createElement('style'); 
+    textBox_a.innerHTML = '.textBox a{color: #a8c7fa!important;}';
+    document.body.append(textBox_a);
+    
+    const userInfoCard_userImage = document.createElement('style'); 
+    userInfoCard_userImage.innerHTML = '#userInfoCard #userImage{border: 0.2rem solid #0f537d;}';
+    document.body.append(userInfoCard_userImage);
+
+    const mobLogInDiv = document.createElement('style'); 
+    mobLogInDiv.innerHTML = '#mobLogInDiv{border: 0.1rem #8b8e8c solid!important; background-color: transparent;}';
+    document.body.append(mobLogInDiv);
+
+    const settingsChoser = document.createElement('style'); 
+    settingsChoser.innerHTML = '.settingsChoser{background-color: #28292a;}';
+    document.body.append(settingsChoser);
+
+    const settingsChoser_span_HOVER = document.createElement('style'); 
+    settingsChoser_span_HOVER.innerHTML = '.settingsChoser span:hover{background-color: #343536;}';
+    document.body.append(settingsChoser_span_HOVER);
+
+    const settingsChoser_active = document.createElement('style'); 
+    settingsChoser_active.innerHTML = '.settingsChoser .active{background-color: #084c74;}';
+    document.body.append(settingsChoser_active);
+
+    const settingsChoser_active_HOVER = document.createElement('style'); 
+    settingsChoser_active_HOVER.innerHTML = '.settingsChoser .active:hover{background-color: #185484;}';
+    document.body.append(settingsChoser_active_HOVER);
+
+    const mainInfo_buyBox = document.createElement('style'); 
+    mainInfo_buyBox.innerHTML = '.mainInfo .buyBox{background-color: #28292a;}';
+    document.body.append(mainInfo_buyBox);
+
+    const mainInfo_buyBox_main_buyButton = document.createElement('style'); 
+    mainInfo_buyBox_main_buyButton.innerHTML = '.mainInfo .buyBox .main .buyButton{background-color: #004a77;}';
+    document.body.append(mainInfo_buyBox_main_buyButton);
+
+    const mainInfo_buyBox_main_buyButton_HOVER = document.createElement('style'); 
+    mainInfo_buyBox_main_buyButton_HOVER.innerHTML = '.mainInfo .buyBox .main .buyButton:hover{background-color: #125680;}';
+    document.body.append(mainInfo_buyBox_main_buyButton_HOVER);
+
+    const storiesCont_div = document.createElement('style'); 
+    storiesCont_div.innerHTML = '.storiesCont div{background-color: #28292a;}';
+    document.body.append(storiesCont_div);
+
+    const storiesCont_div_HOVER = document.createElement('style'); 
+    storiesCont_div_HOVER.innerHTML = '.storiesCont div:hover{background-color: #084c74;}';
+    document.body.append(storiesCont_div_HOVER);
+
+    const text_a = document.createElement('style'); 
+    text_a.innerHTML = '.text a{color: #a8c7fa!important;}';
+    document.body.append(text_a);
 }
 
 if (getCookie(`tema`) == null) {
