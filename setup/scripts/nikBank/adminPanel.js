bbba7a93df0220a99b34f3d84f0b5dce6d1d9a8b
Petar Comp <perinasoba@gmail.com>
2023-11-04 19:26:49 +0100

    VS Code Edit

--- setup/scripts/nikBank/adminPanel.js
+++ setup/scripts/nikBank/adminPanel.js
@@ -33,6 +33,7 @@ if (getCookie(`userCode`) != null) {
         fetch('https://json.extendsclass.com/bin/90aa08ae7a2e')
         .then(response=> response.json())
         .then((allUsers) => {
+            console.log(allUsers); /**/
             document.getElementById(`jasminaBalance`).innerHTML = `${allUsers[0].balance}rsd`;
 
             userList = allUsers;
@@ -108,7 +109,7 @@ function addNewTrans() {
             headers: {
                 'Security-key': 'perinaSoba'
             },
-            body: encodeURIComponent(JSON.stringify(userList)),
+            body: JSON.stringify(userList),
         })
         .then(response=> response.json())
         .then((writeResponse) => {
