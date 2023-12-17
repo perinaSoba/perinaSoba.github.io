14f7e3187c7b89cbd8809718b04a5788385b1c52
Petar Comp <perinasoba@gmail.com>
2023-12-17 22:55:03 +0100

    VS Code Edit

--- setup/scripts/fiskalizacija/fiskalizacija.js
+++ setup/scripts/fiskalizacija/fiskalizacija.js
@@ -1,29 +1,6 @@
 fetch('https://json.extendsclass.com/bin/987bd36c8663')
 .then(response=> response.json())
 .then((allTransData) => {
-    // Cookies
-    function getCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0;i < ca.length;i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-        }
-        return null;
-    }
-
-    // Jwt decoder
-    function decodeJwtResponse(token) {
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-
-        return JSON.parse(jsonPayload);
-    };
-
     function parseURLParams(url) {
         var queryStart = url.indexOf("?") + 1,
             queryEnd   = url.indexOf("#") + 1 || url.length + 1,
@@ -54,6 +31,18 @@ fetch('https://json.extendsclass.com/bin/987bd36c8663')
         document.getElementById("cardInfo").style.display = "none";
     }
 
+    try {
+        var infoPage = urlData.infoPage;
+        var infoPage = infoPage[0];
+    } catch(e) {
+        console.log("pass");
+    }
+
+    if (infoPage == "true") {
+        document.getElementById("sideBar").style.display = "none";
+        document.getElementById("mobNavButt").style.display = "none";
+    }
+    
     var thisTrans = allTransData.filter(function (el) {
         return el.eID == eID;
     });
