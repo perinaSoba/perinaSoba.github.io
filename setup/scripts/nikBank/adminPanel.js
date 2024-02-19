80415a6ac453603a0b92fe93cdd9970020fd3214
Petar Comp <perinasoba@gmail.com>
2024-02-19 21:49:18 +0100

    VS Code Edit

--- setup/scripts/nikBank/adminPanel.js
+++ setup/scripts/nikBank/adminPanel.js
@@ -1,26 +1,3 @@
-// Cookie functions
-/*function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
-// Jwt decoder
-function decodeJwtResponse(token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
-    return JSON.parse(jsonPayload);
-};*/
-
 document.getElementById(`newTransHolder`).style.display = `none`;
 document.getElementById(`userDataHolder`).style.display = `none`;
 
--- setup/scripts/nikForum/spawnStory.js
+++ setup/scripts/nikForum/spawnStory.js
@@ -1,29 +1,6 @@
 fetch('https://perinasoba.github.io/nikForum/setup/stories.json')
 .then(response=> response.json())
 .then((storiesJson) => {
-    // Cookies
-    /*function getCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0;i < ca.length;i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-        }
-        return null;
-    }
-
-    // Jwt decoder
-    function decodeJwtResponse(token) {
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-
-        return JSON.parse(jsonPayload);
-    };*/
-
     /* Spawn story */
     function spawnStory() {
         document.title = "Nik Forum - " + storyObject.title;
